fb908d13d359103fddf2f2652e05c866d9136e66
Spencer Jagernauth <rita.jagernauth01@gmail.com>
2022-03-16 14:28:43 -0400

    new files

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,2 +1,30 @@
 window.alert("This is an alert! JavaScript is running!");
 
+// this creates a function named "fight"
+
+function fight() {
+  window.alert("The fight has begun!");
+}
+fight();
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
+};
+
+fight();
+// create function
+function fight() {
+  window.alert("Welcome to Robot Gladiators!");
+}
+// execute function
+fight();
